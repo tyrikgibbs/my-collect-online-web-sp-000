42c1d9d1d16d19ee8317b88d62a786bfaf49ec4f
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-01 03:19:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,9 @@
 def my_collect(array)
   new_array = []
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
   my_collect(array) do |name|
     new_array << name.split(" ").first
   end
